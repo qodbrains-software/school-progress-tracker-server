edcd42e742066f88382321141fbded1cee8d69ce
vusumzi-qod <vusumzi@qodbrains.com>
2022-06-28 15:47:48 +0200

    login endpoint phase 1 complete

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 import express from 'express';
 const server = express();
 const PORT = 9000;
-import {database} from './data/database.mock.js';
+import { database } from './data/database.mock.js';
+
+server.use(express.json());
  
-server.get("/get-data", (req,res) => {
+server.get("/get-data", (req, res) => {
     res.json(database);
-})
+});
 
-
+server.post("/login", (req, res) =>{
+    const { loginId, password } = req.body;
+    let found = false;
+            
+    database.teachers.forEach(item =>{
+        if(item.loginId === loginId && item.password === password){
+            found = true;
+            return;
+        };
+  });
+        if(found){
+            res.json("teacher login in");
+        }else {
+        res.status(401).json("You are not allowed in here.");
+    };  
+});
+    
+    
 server.listen(PORT, (err) => {
     if(err){
     console.log(`server error`);
-}
+  };
 console.log(`this server is listening to port ${PORT}`);
-})
+});
     
